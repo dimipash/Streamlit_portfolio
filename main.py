26da9851d9bf13fdade5eba14e60c6df8e96a5f8
Dimitar Pashev <dim.pashev@gmail.com>
2025-05-26 09:40:45 +0300

    update chatbot and resume

--- main.py
+++ main.py
@@ -356,7 +356,7 @@ if __name__ == "__main__":
 
 st.markdown(
     """
-<iframe src="https://app.vectorshift.ai/chatbots/embedded/67d84b6f7e6f30aec4d9dd9c?openChatbot=true" width="300px" height="400px" style="border: none; position: fixed; bottom: 0; right: 0; margin: 10px;" allow="clipboard-read; clipboard-write; microphone"/>
+<iframe src="https://app.vectorshift.ai/chatbots/embedded/67d84b6f7e6f30aec4d9dd9c?openChatbot=true" width="500px" height="500px" style="border: 1px solid var(--border-color); position: fixed; bottom: 0; right: 0; margin: 10px; z-index: 1000; background: transparent;" allow="clipboard-read; clipboard-write; microphone"/>
     """,
     unsafe_allow_html=True
 )
